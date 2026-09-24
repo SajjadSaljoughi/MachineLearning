0f3f0d6eaa24a374ec41fd56aeb607952fbb8beb
SajjadSaljoughi <sajjadsaljoghi0925142123@gmail.com>
2026-09-25 01:23:55 +0330

    Add Dollar Rial Price and Tehran House Price

---
+++ Dollar_Rial_Price/LLS.py
@@ -0,0 +1,29 @@
+import numpy as np
+from numpy.linalg import inv
+
+
+class LLS:
+    def __init__(self):
+        self.w = None
+
+    def fit(self, X_train, Y_train):
+        self.w = inv(X_train.T @ X_train) @ X_train.T @ Y_train
+
+    def predict(self, X_test):
+        Y_pred = X_test @ self.w
+        return Y_pred
+
+    def evaluate(self, X_test, Y_test, metric):
+        Y_pred = self.predict(X_test)
+
+        #Mean Absolute Error
+        if metric == 'mae':
+            loss = np.sum(np.abs(Y_test - Y_pred)) / Y_test.shape[0]
+        #Mean Square Error
+        elif metric == 'mse':
+            loss = np.sum((Y_test - Y_pred) ** 2) / Y_test.shape[0]
+        #Root Mean Square Error
+        elif metric == 'rmse':
+            loss = np.sqrt(np.sum((Y_test - Y_pred) ** 2) / Y_test.shape[0])
+
+        return loss
